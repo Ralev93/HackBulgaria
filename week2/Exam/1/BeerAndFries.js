b1b1cb427e4f229ff65c097a82b51c739d594221
Ralev93 <Ralev93@users.noreply.github.com>
2014-06-15 23:32:20 +0300

    Update BeerAndFries.js

--- week2/Exam/1/BeerAndFries.js
+++ week2/Exam/1/BeerAndFries.js
@@ -3,31 +3,31 @@
 
 var beerAndFries = function (items) {
 	var
-		scores = function (type) {
-			var result = [];
+	    scores = function (type) {
+		var result = [];
 
-			items.filter(function (item) {
-				return item.type === type;
-			}).forEach(function (item) {
-				result.push(item.score);
-			});
-
-			return result;
-		},
-
-		descending = function (a,b) {
-			return b - a;
-		},
-
-		result = 0,
-		friesScores = scores("fries").sort(descending),
-		beerScores  = scores("beer").sort(descending);
-
-		beerScores.forEach(function (beerScore,i) {
-			result += beerScore * friesScores[i];
+		items.filter(function (item) {
+		return item.type === type;
+		}).forEach(function (item) {
+			result.push(item.score);
 		});
 
 		return result;
+	    },
+		
+	    descending = function (a,b) {
+		return b - a;
+	    },
+
+	   result = 0,
+	   friesScores = scores("fries").sort(descending),
+	   beerScores  = scores("beer").sort(descending);
+
+	   beerScores.forEach(function (beerScore,i) {
+		result += beerScore * friesScores[i];
+		});
+
+           return result;
 
 };
 
