9a1640f3192fac1f4936cfa232342ee69acbce05
Ralev93 <Ralev93@users.noreply.github.com>
2014-06-18 03:30:53 +0300

    Update app.js

--- week2/Exam/2/app.js
+++ week2/Exam/2/app.js
@@ -1,131 +1,87 @@
 "use strict";
-
 var
-	nIntervId = 0,
-	minuteFstDigit = 0,
-	minuteSndDigit = 0,
-	secondFstDigit = 0,
-	secondSndDigit = 0;
+  nIntervId = 0,
+  minutes = 0,
+  seconds = 0;
 
-var appendTimer = function (secSndDigit) {
-	$("#second-second-digit").text(secSndDigit);
-	$("#second-first-digit").text(secondFstDigit);
-	$("#minute-second-digit").text(minuteSndDigit);
-	$("#minute-first-digit").text(minuteFstDigit);
-}
+var appendTimer = function (min, sec) {
+  if (sec < 10) { 
+    $("#second-second-digit").text(sec.toString()[0]);
+    $("#second-first-digit").text(0);
+  }
+  else {
+    $("#second-second-digit").text(sec.toString()[1]);
+    $("#second-first-digit").text(sec.toString()[0]);
+  }
+  
+  if (min < 10) { 
+    $("#minute-second-digit").text(min.toString()[0]);
+    $("#minute-first-digit").text(0);
+  }
+  else {
+    $("#minute-second-digit").text(min.toString()[1]);
+    $("#minute-first-digit").text(min.toString()[0]);
+  }
+};
 
 function terminate() {
-	 clearInterval(nIntervId);
+  clearInterval(nIntervId);
 };
 
 function begin(f) {
-	nIntervId = setInterval(f, 1000);
-};
-
-function nullarize() {
-	minuteFstDigit = 0;
-	minuteSndDigit = 0;
-	secondFstDigit = 0;
-	secondSndDigit = 0;
+  nIntervId = setInterval(f, 1000);
 };
 
 $(document).ready(function() {
-	$("#minutes").on("click keypress", function () {
-		var minutes = parseInt($("#minutes").val(), 10);
+  $("#minutes").on("click keypress", function () {
+    minutes = parseInt($("#minutes").val(), 10);
+    appendTimer(minutes,seconds);
+  });
 
-		if (minutes < 10) {
-			minuteFstDigit = 0;
-			minuteSndDigit = minutes;
-		}
-		else {
-			minuteFstDigit = Math.floor(minutes/10)%10;
-			minuteSndDigit = minutes%10;
-		}
-		$("#minute-second-digit").text(minuteSndDigit);
-		$("#minute-first-digit").text(minuteFstDigit);
-	});
+  $("#seconds").on("click keypress", function () {
+    seconds = parseInt($("#seconds").val(), 10);
+    appendTimer(minutes,seconds);
+  });
 
-	$("#seconds").on("click keypress", function () {
-		var seconds = parseInt($("#seconds").val(), 10);
+  $("#startDown").on("click", function() {
+    terminate();
+    var 
+	  m = minutes,
+      s = seconds;
+    
+    begin(function () {
+      s -= 1;
+      if (s < 0) {
+        m -= 1;
+        s = 59;
+      }
+      if (m < 0) {
+        m = s = 0;
+        terminate();
+      }
+      appendTimer(m, s);
+    });
+  });
 
-		if (seconds < 10) {
-			secondFstDigit = 0;
-			secondSndDigit = seconds;
-		}
-		else {
-			secondFstDigit = Math.floor(seconds/10)%10;
-			secondSndDigit = seconds%10;
-		}
-		$("#second-second-digit").text(secondSndDigit);
-		$("#second-first-digit").text(secondFstDigit);
-	});
+  $("#startUp").on("click", function() {
+    terminate();
+    var m = 0, s = 0;
+    
+    begin(function () {
+      s += 1;
+      if (s > 59) {
+        m += 1;
+        s = 0;
+      }
+      if (m === minutes && s === seconds) { 
+        terminate();
+      }
+      appendTimer(m, s);
+    });
+  });
 
-
-	$("#startDown").on("click", function() {
-		terminate();
-		
-		begin(function () {
-			if (secondSndDigit < 0) {
-				secondSndDigit = 9;
-				secondFstDigit -= 1;
-			}
-
-			if (secondFstDigit < 0) {
-				secondFstDigit = 5;
-				minuteSndDigit -= 1;
-			}
-
-			if (minuteSndDigit < 0) {
-				minuteSndDigit = 9;
-				minuteFstDigit -= 1;
-			}
-
-			if (minuteFstDigit < 0) {
-				nullarize();
-				terminate();
-			}
-
-			appendTimer(secondSndDigit--);
-		});
-	});
-
-	$("#startUp").on("click", function() {
-		terminate();
-		var fin1 = secondSndDigit,
-			fin2 = secondFstDigit,
-			fin3 = minuteSndDigit,
-			fin4 = minuteFstDigit;
-
-		nullarize();
-
-		begin(function () {
-			if (secondSndDigit >= 10) {
-				secondSndDigit = 0;
-				secondFstDigit += 1;
-			}
-
-			if (secondFstDigit >= 6) {
-				secondFstDigit = 0;
-				minuteSndDigit += 1;
-			}
-			if (minuteSndDigit >= 10) {
-				minuteSndDigit = 0;
-				minuteFstDigit += 1;
-			}
-
-			if (secondSndDigit === fin1 && secondFstDigit === fin2 &&
-				minuteSndDigit === fin3 && minuteFstDigit === fin4) {
-					terminate();
-			}
-
-			appendTimer(secondSndDigit++);
-		});
-	});
-
-	$("#StopAndReset").on("click", function() {
-		terminate();
-		nullarize();
-		appendTimer(0);
-	});
-
+  $("#StopAndReset").on("click", function() {
+    terminate();
+    appendTimer(0,0);
+  });
 });
